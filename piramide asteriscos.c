2f9a49fd6f3b2baebb3028dc3550909c657e6b33
MiguelAR0102 <60169180+MiguelAR0102@users.noreply.github.com>
2020-03-11 12:28:24 -0600

    Update piramide asteriscos.c

--- piramide asteriscos.c
+++ piramide asteriscos.c
@@ -10,7 +10,7 @@ int main (){
         for (int i=0;i<=x;i++){
             for (int j=0;j<=x-i;j++)
             printf(" ");
-            for (int j=0;j<=2*i-1;j++)
+            for (int j=1;j<=2*i-1;j++)
             printf("*");
             printf("\n");
 }
